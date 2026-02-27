63e1b5abc9f184604912840d758e5b37558ec4ef
maxim <fesero@bk.ru>
2026-02-27 13:49:39 +0300

    fixes

--- www/local/routes/api.php
+++ www/local/routes/api.php
@@ -2,7 +2,7 @@
 
 use Bitrix\Main\Routing\RoutingConfigurator;
 use My\Api\Controller\RequestController;
-use My\Api\Controller\Basketcontroller;
+use My\Api\Controller\BasketController;
 use My\Api\Controller\OrderController;
 
 return function (RoutingConfigurator $routes) {
@@ -12,10 +12,10 @@ return function (RoutingConfigurator $routes) {
     $routes->post('/api/v1/request/add', [RequestController::class, 'addAction']);
     $routes->get('/api/v1/request/list', [RequestController::class, 'listAction']);
 
-    $routes->post('/api/v1/basket/add/{productId}/{quantity}', [Basketcontroller::class, 'addAction']);
-    $routes->post('/api/v1/basket/update/{productId}/{quantity}', [Basketcontroller::class, 'updateAction']);
-    $routes->post('/api/v1/basket/delete/{productId}', [Basketcontroller::class, 'deleteAction']);
-    $routes->get('/api/v1/basket/get', [Basketcontroller::class, 'getAction']);
+    $routes->post('/api/v1/basket/add/{productId}/{quantity}', [BasketController::class, 'addAction']);
+    $routes->post('/api/v1/basket/update/{productId}/{quantity}', [BasketController::class, 'updateAction']);
+    $routes->post('/api/v1/basket/delete/{productId}', [BasketController::class, 'deleteAction']);
+    $routes->get('/api/v1/basket/get', [BasketController::class, 'getAction']);
 
     $routes->post('/api/v1/order/create', [OrderController::class, 'createAction']);
 };
